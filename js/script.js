b14b0a0374fdb79e6e0b408684460dd4e5756eaf
Leonardo Silva <leoscruz22@gmail.com>
2022-07-14 10:02:31 -0300

    hamburger menu implementado

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const menubtn = document.querySelector('.hamburguer')
+
+function toggleMenu() {
+    const menu = document.querySelector('.header-container--items')
+    menu.classList.toggle('menu-active')
+
+    if (menu.classList.contains('menu-active')) {
+        menubtn.innerHTML = '<i class="fa-solid fa-2x fa-x x-menu"></i>'
+    } else {
+        menubtn.innerHTML = '<i class="fa-solid fa-2x fa-bars hamburguer-menu"></i>'
+    }
+}
+
+menubtn.addEventListener('click', toggleMenu)
